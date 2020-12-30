443f85c80441bdd7bdf4c3ff1071db6034e52817
GSculerlor <gsculerlor@hotmail.com>
2020-12-30 16:13:45 +0700

    add implementation of tracks

---
+++ Project2020.Game/Models/TrackSong.cs
@@ -0,0 +1,30 @@
+using System;
+using Newtonsoft.Json;
+
+namespace Project2020.Game.Models
+{
+    [Serializable]
+    public class TrackSong
+    {
+        [JsonProperty(@"id")]
+        public int Id;
+
+        [JsonProperty(@"song_name")]
+        public string SongName;
+
+        [JsonProperty(@"song_artist")]
+        public string SongArtist;
+
+        [JsonProperty(@"song_album")]
+        public string SongAlbum;
+
+        [JsonProperty(@"album_path")]
+        public string AlbumPath;
+
+        [JsonProperty(@"track_dir")]
+        public string TrackDir;
+
+        [JsonProperty(@"preview_time")]
+        public long PreviewTime;
+    }
+}
--- Project2020.Game/Project2020GameBase.cs
+++ Project2020.Game/Project2020GameBase.cs
@@ -1,14 +1,22 @@
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.IO.Stores;
+using Project2020.Game.Models;
 using Project2020.Resources;
 
 namespace Project2020.Game
 {
     public class Project2020GameBase : osu.Framework.Game
     {
+        [Cached]
+        protected readonly BindableList<TrackSong> TrackList = new BindableList<TrackSong>();
+
         protected override Container<Drawable> Content { get; }
 
         private DependencyContainer dependencies;
@@ -46,6 +54,15 @@ namespace Project2020.Game
             AddFont(Resources, @"Fonts/Noto-CJK-Basic");
             AddFont(Resources, @"Fonts/Noto-CJK-Compatibility");
             AddFont(Resources, @"Fonts/Noto-Thai");
+
+            loadTrackList();
+        }
+
+        private void loadTrackList()
+        {
+            using (Stream stream = Resources.GetStream("Config/tracklist.json"))
+            using (var sr = new StreamReader(stream))
+                TrackList.AddRange(JsonConvert.DeserializeObject<List<TrackSong>>(sr.ReadToEnd()));
         }
     }
 }
